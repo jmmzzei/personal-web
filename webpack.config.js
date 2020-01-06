65926ae46f9d88ea52081030bc25bfd94a7a9fd6
jmm <josemmazzei@gmail.com>
2020-01-06 04:25:59 -0300

    Inicial commit

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); 
+
+
+module.exports = {
+    entry: path.resolve(__dirname,'./src/js/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    // optimization: {
+    //   minimize: true,
+    //   minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    // },
+    module : {
+        rules: [
+          {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            loader: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'postcss-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name].[ext]',
+            },
+          },
+        ]
+      },
+    plugins:[
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+          filename:'index.html',
+          template: './src/index.html',
+          hash: false,
+            minify: {
+              //all to true
+              collapseWhitespace: false,
+              removeComments: true,
+              removeRedundantAttributes: false,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: false,
+              useShortDoctype: false
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/style.css'
+        }),
+        new webpack.LoaderOptionsPlugin({
+          options: {
+              postcss: [
+                  autoprefixer()
+              ]
+          }
+      }),
+    ]
+}
