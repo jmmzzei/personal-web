8d7dd7d16b64dbe5ccaa9c8f932920525f4bbe10
jmm <josemmazzei@gmail.com>
2020-05-12 16:23:26 -0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,77 @@
-const path = require('path')
+const path = require("path");
 const webpack = require("webpack");
 const autoprefixer = require("autoprefixer");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin'); 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname,'./src/js/index.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    module : {
-        rules: [
-          {
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            loader: [
-              MiniCssExtractPlugin.loader,
-              'css-loader',
-              'postcss-loader',
-              'sass-loader'
-            ]
-          },
-          {
-            test: /\.(png|jpe?g|gif)$/i,
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name].[ext]',
-            },
-          },
+  entry: path.resolve(__dirname, "./src/js/index.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
         ]
       },
-    plugins:[
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-          filename:'index.html',
-          template: './src/index.html',
-          hash: true,
-            minify: {
-              collapseWhitespace: true,
-              removeComments: true,
-              removeRedundantAttributes: true,
-              removeScriptTypeAttributes: true,
-              removeStyleLinkTypeAttributes: true,
-              useShortDoctype: true
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: './css/style.css'
-        }),
-        new webpack.LoaderOptionsPlugin({
-          options: {
-              postcss: [
-                  autoprefixer()
-              ]
-          }
-      }),
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "images",
+          name: "[name].[ext]"
+        }
+      }
     ]
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      hash: true,
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/style.css"
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [autoprefixer()]
+      }
+    })
+  ]
+};
