1cd9220f1edf76e48efd9f45a79c071ad04893ea
jmm <josemmazzei@gmail.com>
2020-05-12 16:24:15 -0300

    Refactor .js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,89 +1,21 @@
-import './../style/main.scss'
+import "./../style/main.scss";
+import { elements } from "./matrix";
 
-console.log(document.getElementById('animate').offsetWidth);
+const applyWaveAnimation = (elementsNumber, elementsPerRow, rows = 35) => {
+  for (let i = rows; i < elementsNumber; i += elementsPerRow) {
+    document.querySelector(
+      `#animate > svg:nth-child(${i})`
+    ).style.animation = `wave 3s ${rows / elementsPerRow}s infinite ease-out`;
+    console.log(rows / elementsPerRow);
+  }
 
-const animate = document.getElementById('animate')
+  rows == 1
+    ? 1
+    : applyWaveAnimation(elementsNumber, elementsPerRow, (rows -= 1));
+};
 
-import Matrix from './matrix'
+applyWaveAnimation(elements.number, elements.perRow, elements.perRow);
 
-const elementsPerRow = 35, 
-      elementsPerColumn = 10,
-      elementSize = animate.offsetWidth/elementsPerRow;
-
-const elementsNumber = elementsPerRow * elementsPerColumn
-
-new Matrix(elementsNumber, elementsPerRow, elementSize)
-
-const rows = 35
-
-console.log(`rows: ${rows}`);
-console.log(`elperrow: ${elementsPerRow}`);
-console.log(rows / elementsPerRow);
-
-function createTag(){
-    let scripty = document.createElement('script')
-    // scripty.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-155317539-1')
-    scripty.setAttribute('style', 'background: #fff; width: 100%; height: 80px;')
-    // scripty.setAttribute('async')
-    document.body.appendChild(scripty);
-    // document.getElementById('body').prepend(scripty)
-}
-
-createTag()
-
-
-
-function wave(elementsNumber, elementsPerRow, rows) {
-
-    for (let i = rows; i < elementsNumber; i += elementsPerRow) {
-        document.querySelector(`#animate > svg:nth-child(${i})`).style.animation = `wave 3s ${rows/elementsPerRow}s infinite ease-out`
-
-        // document.querySelector(`.main > svg:nth-child(${i})`).style.fill = `rgb(2${Math.floor(i/5)},${10 + Math.floor(i/5)},${30 + Math.floor(i/5)})`
-    }
-
-    if (rows == 1) {
-        return 1
-    } else {        
-        return wave(elementsNumber, elementsPerRow, rows -= 1)
-    }
-}
-
-// function js(elementsNumber, rows){
-//     for (let j = 1; j < 12; j++) {
-//         for (let i = j; i < elementsNumber; i += rows ) {
-//             document.querySelector(`#animate > svg:nth-child(${i})`).style.background = `yellow`
-//         }
-//     }
-
-//     for (let k = 44; k < elementsNumber - rows; k += rows) {
-//         document.querySelector(`#animate > svg:nth-child(${k})`).style.background = `black`
-//     }
-
-//     for (let k = 214; k < elementsNumber - rows; k += rows) {
-//         document.querySelector(`#animate > svg:nth-child(${k})`).style.background = `black`
-//     }
-
-//     for (let k = 284; k < 289; k++) {
-//         document.querySelector(`#animate > svg:nth-child(${k})`).style.background = `black`
-//     }
-// }
-
-// js(elementsNumber, elementsPerRow)
-
-
-wave(elementsNumber, elementsPerRow, elementsPerRow)
-
-document.getElementById('submit').addEventListener('click', e => {
-    e.preventDefault()
-})
-
-
-document.getElementById('nav-projects').addEventListener('mouseenter', e => {
-    document.getElementById('projects').firstChild.style.color = '#fff'
-    document.getElementById('projects').firstChild.style.transition = '.4s'
-    
-})
-
-document.getElementById('nav-projects').addEventListener('mouseleave', e => {
-    document.getElementById('projects').firstChild.style.color = 'rgb(56, 56, 56)';
-})
+document.getElementById("submit").addEventListener("click", e => {
+  e.preventDefault();
+});
--- src/js/matrix.js
+++ src/js/matrix.js
@@ -1,17 +1,21 @@
 import Svg from "./svg";
-const anim = document.getElementById('animate')
-console.log(anim);
 
-export default class Matrix {
+export const elements = new (function() {
+    this.perColumn = 10;
+    this.perRow = 35;
+    this.size = animate.offsetWidth / this.perRow;
+    this.number = this.perRow * this.perColumn;
+})();
+
+const Matrix = new (class {
     constructor(quantity, rowElements, elementSize) {
-        this._elementSize = elementSize
-        this._quantity = quantity
-        this._rowElements = rowElements
-        this._totalElements = []
+        this._elementSize = elementSize;
+        this._quantity = quantity;
+        this._rowElements = rowElements;
+        this._totalElements = [];
         for (let index = 0; index < this._quantity; index++) {
-
-            const svg = new Svg(this._elementSize, this._rowElements)
-            this._totalElements.push(svg.svg)
+            const svg = new Svg(this._elementSize, this._rowElements);
+            this._totalElements.push(svg.svg);
         }
     }
-}
+})(elements.number, elements.perRow, elements.size);
