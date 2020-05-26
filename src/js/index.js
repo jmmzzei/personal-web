b0d7e01d386d54a9982f6ccb680cddbbc05295d3
jmm <josemmazzei@gmail.com>
2020-05-26 11:39:17 -0300

    Replace form with contact info

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,19 @@
-import "./../style/main.scss";
-import { elements } from "./matrix";
+import "./../style/main.scss"
+import { elements } from "./elements"
+import { Matrix } from "./matrix"
 
 const applyWaveAnimation = (elementsNumber, elementsPerRow, rows = 35) => {
   for (let i = rows; i < elementsNumber; i += elementsPerRow) {
     document.querySelector(
       `#animate > svg:nth-child(${i})`
-    ).style.animation = `wave 3s ${rows / elementsPerRow}s infinite ease-out`;
-    console.log(rows / elementsPerRow);
+    ).style.animation = `wave 3s ${rows / elementsPerRow}s infinite ease-out`
+    console.log(rows / elementsPerRow)
   }
 
   rows == 1
     ? 1
-    : applyWaveAnimation(elementsNumber, elementsPerRow, (rows -= 1));
-};
+    : applyWaveAnimation(elementsNumber, elementsPerRow, (rows -= 1))
+}
 
-applyWaveAnimation(elements.number, elements.perRow, elements.perRow);
+applyWaveAnimation(elements.number, elements.perRow, elements.perRow)
 
-document.getElementById("submit").addEventListener("click", e => {
-  e.preventDefault();
-});
