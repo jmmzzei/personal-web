5b40ed781946a0cc59755e79824a8bcf0fbdc792
jmm <josemmazzei@gmail.com>
2020-06-17 11:06:20 -0300

    Make the navbar responsive
    Add a language button (without functionality for now) and a link to
    an (empty) about section.

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,24 @@ import { elements } from "./elements"
 import { Matrix } from "./matrix"
 
 const applyWaveAnimation = (elementsNumber, elementsPerRow, rows = 35) => {
-  for (let i = rows; i < elementsNumber; i += elementsPerRow) {
-    document.querySelector(
-      `#animate > svg:nth-child(${i})`
-    ).style.animation = `wave 3s ${rows / elementsPerRow}s infinite ease-out`
-    console.log(rows / elementsPerRow)
-  }
+    for (let i = rows; i < elementsNumber; i += elementsPerRow) {
+        document.querySelector(
+            `#animate > svg:nth-child(${i})`
+        ).style.animation = `wave 3s ${rows / elementsPerRow}s infinite ease-out`
+        console.log(rows / elementsPerRow)
+    }
 
-  rows == 1
-    ? 1
-    : applyWaveAnimation(elementsNumber, elementsPerRow, (rows -= 1))
+    rows == 1
+        ? 1
+        : applyWaveAnimation(elementsNumber, elementsPerRow, (rows -= 1))
 }
 
 applyWaveAnimation(elements.number, elements.perRow, elements.perRow)
 
+
+const menuBtn = document.getElementById('menu')
+const ul = document.getElementById('ul')
+
+menuBtn.addEventListener('click', () => {
+    ul.style.display = ul.style.display == 'none' ? 'flex' : 'none'
+})
