c6508438cef7d5f1be3935d9017be9444092defb
jmm <josemmazzei@gmail.com>
2020-07-05 10:10:02 -0300

    Refactor index.js
    The code was messy so the changeLanguage function was extracted to
    a separate file.

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import "../github64.png"
 import "../icon.png"
 import { elements } from "./elements"
 import { Matrix } from "./matrix"
-import lang from "../lang.json"
+import {changeLanguage} from './changeLanguage'
 
 const applyWaveAnimation = (elementsNumber, elementsPerRow, rows = 35) => {
   for (let i = rows; i < elementsNumber; i += elementsPerRow) {
@@ -24,69 +24,12 @@ document.getElementById("lang").addEventListener("click", e => {
 
   if (currentLang === "en") {
     e.target.childNodes[1].innerText = "es"
-    changeCardLanguage("es")
+    changeLanguage("es")
   } else {
     e.target.childNodes[1].innerText = "en"
-    changeCardLanguage("en")
+    changeLanguage("en")
   }
 })
-
-const cardIds = [
-  "nfc",
-  "cli",
-  "minimal",
-  "twittear",
-  "calendar",
-  "todo",
-  "music",
-  "movie",
-  "almuerzi",
-  "weather",
-  "weatherandroid",
-  "social",
-  "neopixel",
-  "page",
-]
-
-const paragraph = ["first", "second", "third"]
-const navItems = ["about", "projects", "contact"]
-const links = ["code", "download", "guide", "guía", "código", "descargar"]
-
-const linksContainer = document.getElementsByClassName("card__links")
-const arrLinksContainer = Array.from(linksContainer)
-
-const changeCardLanguage = language => {
-  cardIds.forEach(id => {
-    document.getElementById(id).childNodes[1].innerText = lang[language][id].p
-    document.getElementById(id).childNodes[2].innerText =
-      lang[language][id].code
-  })
-
-  navItems.forEach(item => {
-    document.getElementById(item).childNodes[0].innerText =
-      lang[language].nav["nav-" + item]
-    document.getElementById("nav-" + item).childNodes[0].innerText =
-      lang[language].nav["nav-" + item]
-  })
-
-  paragraph.forEach((paragraph, i) => {
-    document.getElementById("aboutcontent").childNodes[i].innerText =
-      lang[language]["aboutcontent"][paragraph]
-  })
-
-  document.getElementById("message").childNodes[0].innerText =
-    lang[language]["message"]
-
-  arrLinksContainer.forEach(container => {
-    let containerArr = Array.from(container.childNodes)
-    containerArr.forEach(e => {
-      if (e.tagName == "A") {
-        let finded = links.find(link => e.innerText == link)
-        if (finded) e.innerText = lang.links[finded]
-      }
-    })
-  })
-}
 
 const menuBtn = document.getElementById("menu")
 const ul = document.getElementById("ul")
