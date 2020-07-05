647a122107bd1fe12030d58dbddbf5f1bd5a8be4
jmm <josemmazzei@gmail.com>
2020-07-05 10:10:02 -0300

    Refactor index.js
    The code was messy so the changeLanguage function was extracted to
    a separate file.

---
+++ src/js/changeLanguage.js
@@ -0,0 +1,56 @@
+import lang from "../lang.json"
+
+const cardIds = [
+  "nfc",
+  "cli",
+  "minimal",
+  "twittear",
+  "calendar",
+  "todo",
+  "music",
+  "movie",
+  "almuerzi",
+  "weather",
+  "weatherandroid",
+  "social",
+  "neopixel",
+  "page",
+]
+
+const paragraph = ["first", "second", "third"]
+const navItems = ["about", "projects", "contact"]
+const links = ["code", "download", "guide", "guía", "código", "descargar"]
+const arrLinksContainer = Array.from(document.getElementsByClassName("card__links"))
+
+export const changeLanguage = language => {
+  cardIds.forEach(id => {
+    document.getElementById(id).childNodes[1].innerText = lang[language][id].p
+    document.getElementById(id).childNodes[2].innerText =
+      lang[language][id].code
+  })
+
+  navItems.forEach(item => {
+    document.getElementById(item).childNodes[0].innerText =
+      lang[language].nav["nav-" + item]
+    document.getElementById("nav-" + item).childNodes[0].innerText =
+      lang[language].nav["nav-" + item]
+  })
+
+  paragraph.forEach((paragraph, i) => {
+    document.getElementById("aboutcontent").childNodes[i].innerText =
+      lang[language]["aboutcontent"][paragraph]
+  })
+
+  document.getElementById("message").childNodes[0].innerText =
+    lang[language]["message"]
+
+  arrLinksContainer.forEach(container => {
+    let containersArr = Array.from(container.childNodes)
+    containersArr.forEach(e => {
+      if (e.tagName == "A") {
+        let finded = links.find(link => e.innerText == link)
+        if (finded) e.innerText = lang.links[finded]
+      }
+    })
+  })
+}
