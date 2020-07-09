d7c41782546a0746695f4f3a02613b03baeaeca9
jmm <josemmazzei@gmail.com>
2020-07-09 20:25:18 -0300

    Add ts-graphql-api card

--- src/js/changeLanguage.js
+++ src/js/changeLanguage.js
@@ -1,28 +1,31 @@
-import lang from "../lang.json"
-
-const cardIds = [
-  "nfc",
-  "cli",
-  "minimal",
-  "twittear",
-  "calendar",
-  "todo",
-  "music",
-  "movie",
-  "almuerzi",
-  "weather",
-  "weatherandroid",
-  "social",
-  "neopixel",
-  "page",
-]
-
-const paragraph = ["first", "second", "third"]
-const navItems = ["about", "projects", "contact"]
-const links = ["code", "download", "guide", "guía", "código", "descargar"]
-const arrLinksContainer = Array.from(document.getElementsByClassName("card__links"))
+import lang from '../lang.json'
 
 export const changeLanguage = language => {
+  const cardIds = [
+    'nfc',
+    'graph',
+    'cli',
+    'minimal',
+    'twittear',
+    'calendar',
+    'todo',
+    'music',
+    'movie',
+    'almuerzi',
+    'weather',
+    'weatherandroid',
+    'social',
+    'neopixel',
+    'page',
+  ]
+
+  const paragraph = ['first', 'second', 'third']
+  const navItems = ['about', 'projects', 'contact']
+  const links = ['code', 'download', 'guide', 'guía', 'código', 'descargar']
+  const arrLinksContainer = Array.from(
+    document.getElementsByClassName('card__links'),
+  )
+  
   cardIds.forEach(id => {
     document.getElementById(id).childNodes[1].innerText = lang[language][id].p
     document.getElementById(id).childNodes[2].innerText =
@@ -31,23 +34,23 @@ export const changeLanguage = language => {
 
   navItems.forEach(item => {
     document.getElementById(item).childNodes[0].innerText =
-      lang[language].nav["nav-" + item]
-    document.getElementById("nav-" + item).childNodes[0].innerText =
-      lang[language].nav["nav-" + item]
+      lang[language].nav['nav-' + item]
+    document.getElementById('nav-' + item).childNodes[0].innerText =
+      lang[language].nav['nav-' + item]
   })
 
   paragraph.forEach((paragraph, i) => {
-    document.getElementById("aboutcontent").childNodes[i].innerText =
-      lang[language]["aboutcontent"][paragraph]
+    document.getElementById('aboutcontent').childNodes[i].innerText =
+      lang[language]['aboutcontent'][paragraph]
   })
 
-  document.getElementById("message").childNodes[0].innerText =
-    lang[language]["message"]
+  document.getElementById('message').childNodes[0].innerText =
+    lang[language]['message']
 
   arrLinksContainer.forEach(container => {
     let containersArr = Array.from(container.childNodes)
     containersArr.forEach(e => {
-      if (e.tagName == "A") {
+      if (e.tagName == 'A') {
         let finded = links.find(link => e.innerText == link)
         if (finded) e.innerText = lang.links[finded]
       }
